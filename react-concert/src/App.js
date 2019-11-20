525a0c69959ff8b1c368471fffdcd684b5cdf806
lucrampro <lucienramassamy@gmail.com>
2019-11-20 18:15:06 +0100

    mise en route du react router

--- react-concert/src/App.js
+++ react-concert/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import des components
+import Menu from "./components/Menu";
+// Classe App
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Menu />
+        <Route path="/">
+            <App />
+        </Route>
+      </Router>
     </div>
   );
 }
